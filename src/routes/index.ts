324ba6b8da9575cb7ca2cf1f1e51ca0c8e0ca183
Daniel Belarmino <belarba@gmail.com>
2020-07-12 11:31:48 -0300

    using request params

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'blablabla' });
+routes.get('/feriados/:local/:name', (request, response) => {
+  return response.json({ localidade: request.params.local, nome: request.params.name });
 });
 
 export default routes;
